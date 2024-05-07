1b767cf3778d1576c0cb7beab90a81ea06c636cc
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-07 20:26:55 +0200

    fine

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const listaSpesa = ['pollo','legumi','verdure','trenoboloneacetato','acqua'];
+
+const ul = document.getElementById('lista');
+
+
+for (let i = 0; i < listaSpesa.length; i++) {
+
+    const li = document.createElement('li');
+
+    li.innerText = listaSpesa[i];
+    ul.append(li);
+
+};
+
+console.log(listaSpesa[1]);
+
